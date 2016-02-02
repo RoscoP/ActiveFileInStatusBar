19be83b99b5157560c2da80bbe2a27d164d852d7
Ralph Lewis <RoscoP@users.noreply.github.com>
2016-02-01 23:17:10 -0800

    Renamed and tweaked.

--- extension.js
+++ extension.js
@@ -8,7 +8,7 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "slnworkspace" is now active!'); 
+	console.log('Congratulations, your extension "ActiveFileInStatusBar" is now active!'); 
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
@@ -16,11 +16,10 @@ function activate(context) {
 	// var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
 		// The code you place here will be executed every time your command is executed
 
-        var sb = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -1000000);
+        var sb = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10000);
         sb.text = '';
         sb.command = 'workbench.action.files.copyPathOfActiveFile'
         sb.tooltip = 'Copy active file to clipboard'
-        sb.show();
         
         // vscode.workspace.onDidOpenTextDocument( function textEditorChange(e){
         //     console.log("DocOpen  : " + e.fileName);
@@ -36,7 +35,14 @@ function activate(context) {
         // });
         vscode.window.onDidChangeActiveTextEditor( function textEditorChange(e){
             console.log("TextEdit : " + e.document.fileName);
-            sb.text = e.document.isUntitled ? '' : e.document.fileName;
+            if( e.document.isUntitled ){
+                sb.text = '';
+                sb.hide();    
+            }
+            else {
+                sb.text = e.document.fileName
+                sb.show();    
+            }
         });
 
 	context.subscriptions.push(sb);
