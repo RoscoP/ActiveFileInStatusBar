1ff8cd992ef8e16dc2511aa77ae8e26dd60ea338
Ralph Lewis <RoscoP@users.noreply.github.com>
2016-02-02 22:43:58 -0800

    Added icons, allow reveal in OS

--- extension.js
+++ extension.js
@@ -5,12 +5,6 @@ var i = 0;
 var sb = null;
 
 function OnCommand( textEditor ) {
-    
-    var config = vscode.workspace.getConfiguration('ActiveFileInStatusBar');
-    var cmd = vscode.commands.getCommands();
-    cmd.then(function(a,b,c){
-        console.log(a);
-    });
 }
 
 function OnStatusBarUpdate( textEditor ) {
@@ -25,7 +19,13 @@ function OnStatusBarUpdate( textEditor ) {
             if (!config.fullpath && vscode.workspace.rootPath){
                 filePath = path.relative(vscode.workspace.rootPath, textEditor.document.fileName);
             }
-            sb.text = filePath;
+            var icon = '$(clippy)';
+            sb.tooltip = 'Copy active file to clipboard';
+            if (config.revealFile) {
+                icon = '$(file-submodule)';
+                sb.tooltip = 'Reveal file';
+            }
+            sb.text = icon + ' ' + filePath;
             sb.show();
         }
         console.log("TextEdit : " + sb.text);
@@ -33,23 +33,30 @@ function OnStatusBarUpdate( textEditor ) {
 }
 
 function CreateStatusBar() {
-    vscode.commands.registerTextEditorCommand("extension.ActiveFileInStatusBar.OnCommand", OnCommand);
+    var config = vscode.workspace.getConfiguration('ActiveFileInStatusBar');
+    // vscode.commands.registerTextEditorCommand("extension.ActiveFileInStatusBar.OnCommand", OnCommand);
     var sb = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
     sb.text = '';
-    sb.command = "extension.ActiveFileInStatusBar.OnCommand"; // 'workbench.action.files.copyPathOfActiveFile'
-    sb.tooltip = 'Copy active file to clipboard';
+    sb.command = 'workbench.action.files.copyPathOfActiveFile';
+    if (config.revealFile) {
+        sb.command = 'workbench.action.files.revealActiveFileInWindows';
+    }
     return sb;
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    
-    sb = CreateStatusBar();
-    vscode.window.onDidChangeActiveTextEditor( OnStatusBarUpdate );
-    OnStatusBarUpdate( vscode.window.activeTextEditor );
 
-	context.subscriptions.push(sb);
+    var config = vscode.workspace.getConfiguration('ActiveFileInStatusBar');
+    if (config.enable) {
+        sb = CreateStatusBar();
+        vscode.window.onDidChangeActiveTextEditor( OnStatusBarUpdate );
+        OnStatusBarUpdate( vscode.window.activeTextEditor );
+
+        context.subscriptions.push(sb);
+    }
+
 }
 exports.activate = activate;
 
