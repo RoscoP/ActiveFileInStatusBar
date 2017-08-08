98375f211e7e075bbdbbf52f97364013035e6ea1
Ralph Lewis <RoscoP@users.noreply.github.com>
2017-08-07 23:22:40 -0700

    Add option for color text, LICENSE, and CHANGELOG. Fixes: #1, #2, #4, #7

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+
+## [1.0.3] - 2017-08-08
+### Fixed
+- [#1](https://github.com/RoscoP/ActiveFileInStatusBar/issues/1) - Copy to clipboard doesn't respect fullPath setting
+- [#2](https://github.com/RoscoP/ActiveFileInStatusBar/issues/2) - Update configuration on config changed instead of file open
+- [#4](https://github.com/RoscoP/ActiveFileInStatusBar/issues/4) - Force display of absolute path when file is outside of opened folder
+- [#7](https://github.com/RoscoP/ActiveFileInStatusBar/issues/7) - The last active file is still shown even if there is no active editor
+
+### Changed
+- Removed SVG marketplace icon as per changes to the [VSCode Marketplace requirements](https://code.visualstudio.com/docs/extensions/publish-extension#_usage).
+
+### Added
+- Configuration `ActiveFileInStatusBar.color` for the color of the text of the file name in the statusbar.
+- Changelog file.
+- License file.
--- extension.js
+++ extension.js
@@ -1,27 +1,29 @@
 var vscode = require('vscode');
+var copypaste = require('copy-paste');
 var path = require('path');
 
 var sb = null;
 
 function OnStatusBarUpdate( textEditor ) {
+    textEditor = textEditor ? textEditor : vscode.window.activeTextEditor;
     if( textEditor ){
         var config = vscode.workspace.getConfiguration('ActiveFileInStatusBar');
-        if( textEditor.document.isUntitled ){
+        if( !textEditor.document || textEditor.document.isUntitled ){
             sb.text = '';
             sb.hide();
         }
         else {
             var filePath = textEditor.document.fileName;
-            if (!config.fullpath && vscode.workspace.rootPath){
-                filePath = path.relative(vscode.workspace.rootPath, textEditor.document.fileName);
+            if (!config.fullpath){
+                filePath = vscode.workspace.asRelativePath(textEditor.document.fileName)
+                filePath = path.normalize(filePath)
             }
-            var icon = '$(clippy)';
             sb.tooltip = 'Copy active file to clipboard';
             if (config.revealFile) {
-                icon = '$(file-submodule)';
                 sb.tooltip = 'Reveal file';
             }
-            sb.text = icon + ' ' + filePath;
+            sb.color = config.color;
+            sb.text = filePath;
             sb.show();
         }
     }
@@ -29,27 +31,38 @@ function OnStatusBarUpdate( textEditor ) {
 
 function CreateStatusBar() {
     var config = vscode.workspace.getConfiguration('ActiveFileInStatusBar');
-    var sb = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    var sb = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -1);
     sb.text = '';
-    sb.command = 'workbench.action.files.copyPathOfActiveFile';
-    if (config.revealFile) {
-        sb.command = 'workbench.action.files.revealActiveFileInWindows';
-    }
+    sb.command = 'extension.ActiveFileInStatusBarClicked';
     return sb;
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
     var config = vscode.workspace.getConfiguration('ActiveFileInStatusBar');
     if (config.enable) {
         sb = CreateStatusBar();
         vscode.window.onDidChangeActiveTextEditor( OnStatusBarUpdate );
+        vscode.workspace.onDidChangeConfiguration( OnStatusBarUpdate );
         OnStatusBarUpdate( vscode.window.activeTextEditor );
 
         context.subscriptions.push(sb);
     }
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    var disposable = vscode.commands.registerCommand('extension.ActiveFileInStatusBarClicked', function (args) {
+        var config = vscode.workspace.getConfiguration('ActiveFileInStatusBar');
+        if (config.revealFile){
+            vscode.commands.executeCommand('workbench.action.files.revealActiveFileInWindows')
+        }
+        else {
+            copypaste.copy(sb.text)
+        }
+    });
+    context.subscriptions.push(disposable);
 
 }
 exports.activate = activate;
